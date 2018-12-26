c84517b7e2d48e7b91f6083d741b6e63fc9b9622
khacsinhcs <khacsinhcs@gmail.com>
2018-12-26 22:07:41 +0700

    update demo model use mimix

--- src/test/scala/model.scala
+++ src/test/scala/model.scala
@@ -3,13 +3,18 @@ implicit val typeSystem = new TypeSystem()
 
 trait HasName extends HasType {
   val first_name = f("first_name", "First name", StringType)
-  val last_name = f("first_name", "Last name", StringType)
+  val last_name = f("last_name", "Last name", StringType)
 }
 
-object Student extends Type(name = "Student", description = "Student") {
-  val first_name = f("first_name", "First name", StringType)
-  val last_name = f("first_name", "Last name", StringType)
+trait HasPhone extends HasType {
+  val phone = f("phone", "Phone number", required = false, PhoneType)
 }
 
-object Teacher extends Type(name = "Teacher", description = "He teach student") {
+trait HasContactInfo extends HasName with HasPhone
+
+object Student extends Type(name = "Student", description = "Student") with HasContactInfo {
+  val age = f("age", StringType)
+}
+
+object Teacher extends Type(name = "Teacher", description = "He teach student") with HasContactInfo {
 }
